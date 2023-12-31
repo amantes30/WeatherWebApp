8d401e6c5e209104f871faeac2bef1294ec73a47
amantes30 <49632606+amantes30@users.noreply.github.com>
2024-01-01 04:04:43 +0800

    final_commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const axios = require("axios");
 const ejs = require('ejs');
-
+const fs = require('fs').promises;
+const { resolve } = require("path");
 
 const app = express();
 const port = 3000;
@@ -18,11 +19,30 @@ async function getCountryList(){
             countrylist.push(countryInfo);
         });
         countrylist.forEach(e => {
-            console.log(e);
+           // console.log(e);
         })
         
         
     })
+}
+async function getCountryList_csv(){
+    let infoList = [];
+    try {
+        const data = await fs.readFile('./city_coordinates.csv', 'utf8');
+        const infoList = data.split('\r');
+
+        const countrylist = infoList.map(e => ({
+            name: `${e.split(',')[2]}, ${e.split(',')[3]}`,
+            latlng: `${e.split(',')[0]}, ${e.split(',')[1]}`,
+        })).filter(e => e.latlng !== "latitude, longitude");  // Adjust the filter condition as needed
+
+        return countrylist;
+    } catch (err) {
+        console.error(err);
+        throw err;  // Propagate the error so it can be caught in the calling code
+    }
+    
+   
 }
 
 
@@ -30,8 +50,14 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.get('/', async (req, res)=>{
-    await getCountryList();
-    res.render('index', { countrylist });
+    try {
+        const countrylist = await getCountryList_csv();
+        res.render('index', { countrylist });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+  
 })
 app.get('/:countryname', (req, res)=>{
     const {countryname} = req.params;
